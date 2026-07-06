9166f63d66b736774bab14b5eff6cdb8b68bde03
Renitaparumal <renitaparumal26@gmail.com>
2026-07-06 11:52:30 +0200

    Update milestone_01.py
    Added dataset loader

--- data/milestone_01.py
+++ data/milestone_01.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+try:
+    df = pd.read_csv("data/finflow_users.csv")
+except FileNotFoundError:
+    print("Error: data/finflow_users.csv not found.")
+    exit()
+
+print("Dataset loaded successfully.")
